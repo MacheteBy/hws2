6d34916dd77fdd0925c15a0d2095580c680e6dbd
MacheteBy <Macheteby@gmail.com>
2024-02-24 02:06:15 +0300

    HW8 24.02.24

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -7,12 +7,18 @@ type ActionType =
 export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            let newState = [...state]
+            if(action.payload === 'up'){
+                newState.sort((a: any, b: any) => a.name > b.name ? 1 : -1)
+            }
+            if(action.payload === 'down'){
+                newState.sort((a: any, b: any) => a.name < b.name ? 1 : -1)
+            }
+            console.log(newState)
+            return newState // need to fix
         }
         case 'check': {
-
-            return state // need to fix
+            return  [...state].filter((t: any) => t.age >= action.payload)// need to fix
         }
         default:
             return state
