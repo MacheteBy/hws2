479db392402e0335f6a88e15b8dc8a30fb0b13f9
Machete <macheteby@gmail.com>
2024-05-02 22:20:22 +0300

    HW13 02.05.24

--- src/s2-homeworks/hw05/pages/PreJunior.tsx
+++ src/s2-homeworks/hw05/pages/PreJunior.tsx
@@ -3,6 +3,7 @@ import HW1 from '../../hw01/HW1'
 import HW2 from '../../hw02/HW2'
 import HW3 from '../../hw03/HW3'
 import HW4 from '../../hw04/HW4'
+import HW13 from '../../hw13/HW13'
 
 function PreJunior() {
     return (
@@ -12,6 +13,7 @@ function PreJunior() {
             <HW2 />
             <HW3 />
             <HW4 />
+            <HW13 />
         </div>
     )
 }
