81c94f3867b9734c3d9dcb6421e96dd2cc2ed728
MacheteBy <Macheteby@gmail.com>
2024-06-03 22:50:17 +0300

    HW15 fix 03.06.24

--- src/s2-homeworks/hw15/common/c10-SuperSort/SuperSort.tsx
+++ src/s2-homeworks/hw15/common/c10-SuperSort/SuperSort.tsx
@@ -16,13 +16,13 @@ export const pureChange = (sort: string, down: string, up: string) => {
     // пишет студент, sort: (click) => down (click) => up (click) => '' (click) => down ...
     switch (sort) {
         case '':
-            return up
-        case up:
             return down
         case down:
+            return up
+        case up:
             return ''
         default:
-            return up
+            return down
     }
 }
 
